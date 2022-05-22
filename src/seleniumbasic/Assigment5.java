e05e56f866fd9963b408e42abd042e6bd14b4c51
faruk ayaz <46282512+frkyz07@users.noreply.github.com>
2022-05-22 15:52:08 +0300

    frames assignment

---
+++ src/seleniumbasic/Assigment5.java
@@ -0,0 +1,27 @@
+package seleniumbasic;
+
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Assigment5 {
+
+	public static void main(String[] args) {
+
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\frkyz\\OneDrive\\Belgeler\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://the-internet.herokuapp.com/");
+		driver.findElement(By.linkText("Nested Frames")).click();
+		driver.switchTo().frame("frame-top");
+		driver.switchTo().frame("frame-middle");
+		System.out.println(driver.findElement(By.id("content")).getText());
+		
+
+	}
+
+}
